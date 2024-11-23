38aa4f3e4c9c3b9282238be98e173fbed688d141
Elhadj Tall <elhadjoumartall10@gmail.com>
2024-11-23 23:06:32 +0100

    Ajout du NavItems

--- frontend/src/components/NavItems.jsx
+++ frontend/src/components/NavItems.jsx
@@ -1,27 +1,31 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
-
-
 const NavItems = () => {
+    const [menuToggle, setMenuToggle] = useState(false);
+    const [socialToggle, setSocialToggle] = useState(false);
+    const [headerFixed, setHeaderFixed] = useState(false);
+
     return (
-        <div className="menu-area">
-           <div className="menu">
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/services">Services</Link></li>
-                <li><Link to="/pages">Pages</Link></li>
-                <li><Link to="/contact">Contact</Link></li>
-            </ul>
-           </div>
+        <header className={`header-section style-4 ${headerFixed ? 'header-fixed fadeInUp' : ''}`}>
+            <div className="menu-area">
+                <div className="menu">
+                    <ul className={`lab-ul ${menuToggle ? 'active' : ''}`}>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/about">About</Link></li>
+                        <li><Link to="/services">Services</Link></li>
+                        <li><Link to="/pages">Pages</Link></li>
+                        <li><Link to="/contact">Contact</Link></li>
+                    </ul>
+                </div>
 
-           {/* <!-- Mobile Menu --> */}
-           <div className="ellepsis-bar d-md-none" onClick={() => setSocialToggle(!socialToggle)}>
-            <i className="icofont-info-square"></i>
-           </div>
-        </div>
-    )
-}
+                {/* Mobile Menu */}
+                <div className="ellepsis-bar d-md-none" onClick={() => setSocialToggle(!socialToggle)}>
+                    <i className="icofont-info-square"></i>
+                </div>
+            </div>
+        </header>
+    );
+};
 
-export default NavItems
+export default NavItems;
